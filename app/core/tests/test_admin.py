092030d7645b239e8770ffd872d437a82c6da62a
King George <kpgpivotal@gmail.com>
2022-01-25 17:49:05 +0530

    User customaisation code

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,28 @@
+from django.test import TestCase, Client
+from django.contrib.auth  import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@palace.com',
+            password = 'dsfkhldfdgf'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@work.com',
+            password = 'dsfkhldfdgf',
+            name = 'Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test the users that are listed """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
