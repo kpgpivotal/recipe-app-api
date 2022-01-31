6a185c2e1f095f20fee1981a4f7ad90a742513c2
King George <kpgpivotal@gmail.com>
2022-01-31 15:02:15 +0530

    Updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,7 +8,7 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
-    
+
     fieldsets = (
     (None, {'fields': ('email', 'password')}),
     (_('Personal Info'), {'fields': ('name',)}),
@@ -23,6 +23,16 @@ class UserAdmin(BaseUserAdmin):
         }
     ),
     (_('Important dates'), {'fields': ('last_login',)}),
-)
+
+
+    )
+
+    add_fieldsets = (
+        ( None, {
+            'classes' : ('wide',),
+            'fields': ('email', 'password1', 'password2')
+                    }
+        ),
+    )
 
 admin.site.register(models.User, UserAdmin)
